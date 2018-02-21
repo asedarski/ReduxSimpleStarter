c7930356f7283233e612bda8c8518d2d92d76fb3
Alicia Sedarski <alicia.anne.ktr@gmail.com>
2018-02-21 15:01:55 -0500

    Basic search box implementation as a 'functional component' that has no ability to be introspective

--- src/index.js
+++ src/index.js
@@ -1,27 +1,14 @@
-// Import React module from node_modlues
+import {YOUTUBE_API_KEY} from 'react-native-dotenv';
 import React from 'react';
-// React and ReactDOM have diverged, use ReactDOM to interact with the DOM,
-// React is used to create and manage components
 import ReactDOM from 'react-dom';
+import SearchBar from './components/search_bar';
 
-// Create a new component. This component should produce some HTML
-    // const = Constant, the value will never change
-    // function() {} === () => {} "fat arrow" to define functions
 const App = () => {
-    // JSX - javascript dialect - looks like HTML but is really just javascript
-    // Webpack and babel are going to transpose this to javascript
-    return <div>Hi!</div>;
-
-    // How it looks after it's translated into vanilla JS (babeljs.io/repl)
-    // return React.createElement(
-    //      "div",
-    //      null,
-    //      "Hi!"
-    // );
+    return (
+        <div>
+            <SearchBar />
+        </div>
+    );
 }
 
-// Take this components generated HTML and put it on the page (in the DOM)
-    // We need to create an instance of the App in order to render to the DOM
-    // The "HTML" looking syntax actually creates an instance of the class
-    // The second argument is the target for the rendering
 ReactDOM.render(<App />, document.querySelector('.container'));
