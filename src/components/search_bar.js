c2fbd8c661b2cec89395fb02be54f4b0f24fd684
Alicia Sedarski <alicia.anne.ktr@gmail.com>
2018-02-22 16:15:26 -0500

    Basic search bar with component
    This component is class based, a controlled component. This means that the component
    gets its info from the state of the component, instead of the component dictating the state.
    ie. the value of the input is determined by the state of the component, instead of the value
    of the input dictating the state of the component.

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,8 +1,23 @@
 import React, { Component } from 'react';
 
 class SearchBar extends Component {
+    constructor(props) {
+        // Call parent constructor
+        super(props);
+
+        // Set the state of the component. Initialize the term property to '' to be updated on change
+        // when the state changes, the component and any children re-render
+        this.state = { term: '' };
+    }
+
     render() {
-        return <input />;
+        return (
+            <div>
+                <input
+                    value={this.state.term}
+                    onChange={event => this.setState({ term: event.target.value })} />
+            </div>
+        );
     }
 }
 
