16d4861c3a32d083fc9d56bdbe2316c6de9396a0
Alicia Sedarski <alicia.anne.ktr@gmail.com>
2018-02-26 12:11:29 -0500

    Basic video list

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
 
 class App extends Component {
     constructor(props) {
@@ -20,6 +21,7 @@ class App extends Component {
         return (
             <div>
                 <SearchBar />
+                <VideoList videos={this.state.videos} />
             </div>
         );
     }
