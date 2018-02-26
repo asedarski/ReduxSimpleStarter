90f9ebc8adacd7bfbb6a7a9d13223b7c8b682910
Alicia Sedarski <alicia.anne.ktr@gmail.com>
2018-02-26 13:46:19 -0500

    Functional input bar and throttled search

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import { YOUTUBE_API_KEY } from 'react-native-dotenv';
 import React, { Component } from 'react';
+import _ from 'lodash';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
@@ -10,20 +11,35 @@ class App extends Component {
     constructor(props) {
         super(props);
 
-        this.state = { videos: [] };
+        this.state = {
+            videos: [],
+            selectedVideo: null
+        };
 
-        YTSearch({key: YOUTUBE_API_KEY, term: 'surfboards'}, (videos) => {
+        this.videoSearch('surfboards');
+    }
+
+    videoSearch(term) {
+        YTSearch({key: YOUTUBE_API_KEY, term: term}, (videos) => {
             // { videos: videos } = { videos }
-            this.setState({ videos });
+            this.setState({
+                videos: videos,
+                selectedVideo: videos[0]
+            });
         });
     }
 
     render() {
+        // Throttles the search bar, it can only trigger once every 300 miliseconds
+        const videoSearch = _.debounce((term) => { this.videoSearch(term) }, 300);
+
         return (
             <div>
-                <SearchBar />
-                <VideoDetail video={this.state.videos[0]}/>
-                <VideoList videos={this.state.videos} />
+                <SearchBar onSearchTermChange={videoSearch} />
+                <VideoDetail video={this.state.selectedVideo} />
+                <VideoList
+                    onVideoSelect={selectedVideo => (this.setState({selectedVideo}))}
+                    videos={this.state.videos} />
             </div>
         );
     }
